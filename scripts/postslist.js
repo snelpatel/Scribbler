6f660c98472c8cd9a1d02e9ef5d0eb95eed73139
Snehal Patel <snelpatel@gmail.com>
2021-03-03 11:18:22 +0530

    added functionality to trash a post via modal confirmation box

---
+++ scripts/postslist.js
@@ -0,0 +1,49 @@
+//search the modals and obtain the reference here
+var deletePostModal = document.getElementById("deletePostModal");
+var cancelDelete = document.getElementById("cancelDelete");
+var confirmDelete = document.getElementById("confirmDelete");
+
+//search for the delete icons that opened the modal
+var deleteIcons = document.querySelectorAll(".delete-icon");
+var deleteIconsArray = Array.from(deleteIcons);
+
+//delete the seleted post
+var selectedPost;
+openDeletePostModal = (deletePostModal, clickedIcon) => {
+  selectedPost = "";
+  openModal(deletePostModal);
+  selectedPost = clickedIcon.closest(".post");
+};
+
+deletePost = () => {
+  selectedPost.remove();
+  closeModal(deletePostModal);
+};
+
+//Event handlers
+deleteIconsArray.map(deleteIcon => {
+    deleteIcon.addEventListener("click", e =>
+      openDeletePostModal(deletePostModal, e.target)
+    );
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target == deletePostModal) {
+      deletePostModal.style.display = "none";
+    }
+});
+
+cancelDelete.addEventListener("click", () => closeModal(deletePostModal));
+confirmDelete.addEventListener("click", () => deletePost());
+
+// Updating Session storage before redirecting to post.html page to view the clicked post
+navigateToPost = postThread => {
+    var post = postThread.closest(".post");
+    var author = post.querySelector(".username p").textContent.trim();
+    var postTitle = post.querySelector(".title p").textContent.trim();
+    var postContent = post.querySelector(".content").textContent.trim();
+    sessionStorage.setItem("author", author);
+    sessionStorage.setItem("postTitle", postTitle);
+    sessionStorage.setItem("postContent", postContent);
+    window.location.href = "../html/post.html";
+};
