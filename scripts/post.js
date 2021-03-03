01012d75fb25ad4147e9f2ad37367e465d60ae0e
Snehal Patel <snelpatel@gmail.com>
2021-03-03 12:05:36 +0530

    edit & save post functionality added to post.html page

--- scripts/post.js
+++ scripts/post.js
@@ -8,3 +8,34 @@ window.onload = function() {
     titleNode.innerHTML = sessionStorage.getItem("postTitle");
     contentNode.innerHTML = sessionStorage.getItem("postContent");
 };
+
+document.addEventListener("click",function(e) {
+    if (hasClass(e.target, "saved")) {
+      //When edit button is clicked
+      e.target.classList.remove("saved");
+      e.target.classList.add("unsaved");
+      e.target.innerHTML = 'Save <i class="fa fa-save"></i>';
+      titleNode.setAttribute("contenteditable", true);
+      titleNode.classList.add("editable");
+      contentNode.setAttribute("contenteditable", true);
+      contentNode.classList.add("editable");
+    }
+    else if (hasClass(e.target, "unsaved")) {
+        //When save button is clicked
+        e.target.classList.remove("unsaved");
+        e.target.classList.add("saved");
+        e.target.innerHTML = 'Edit <i class="fa fa-edit"></i>';
+        titleNode.setAttribute("contenteditable", false);
+        titleNode.classList.remove("editable");
+        contentNode.setAttribute("contenteditable", false);
+        contentNode.classList.remove("editable");
+        
+        // Updating Session storage on Save to reflect correct data if only this page is refreshed
+        sessionStorage.setItem("postTitle", titleNode.innerText);
+        sessionStorage.setItem("postContent", contentNode.innerText);
+    }    
+}, false);
+
+function hasClass(elem, className) {
+  return elem.classList.contains(className);
+}
